0cd86c7d71f02ee26a7b9a912fdeae3a4f333a94
ravindudinushan <ravindudinushan9966@gmail.com>
2024-10-25 17:04:27 +0530

    update

--- src/views/components/Footer/Footer.tsx
+++ src/views/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ export function Footer() {
   return (
     <footer className="bg-bg text-white p-3 w-full fixed bottom-0">
       <div className="container mx-auto text-center">
-        <h6 className="text-sm md:text-base">© 2024 All Right Reserved | Designed By{" "}
+        <h6 className="text-sm md:text-base">&copy; {new Date().getFullYear()} All Right Reserved | Designed By{" "}
           <a href="https://github.com/ravindudinushan" target="_blank" rel="noopener noreferrer"
             className="font-semibold text-primary transition-colors duration-300">Ravindu Dinshan</a>
         </h6>
