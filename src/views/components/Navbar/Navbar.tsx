5eca2de2c44770ece0d6884f16c4e1d57928ea4e
ravindudinushan <ravindudinushan9966@gmail.com>
2024-10-25 15:36:24 +0530

    colour

--- src/views/components/Navbar/Navbar.tsx
+++ src/views/components/Navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import logo from "../../../assets/images/logo.png";
+import logo from "../../../assets/images/logo1.png";
 import "./Navbar.css";
 
 export function Navbar() {
